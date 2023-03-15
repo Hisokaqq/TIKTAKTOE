2ba2170ebb48ca29353da64cb8c90cca9ea3ae60
Alex <baklan4ik1993@gmail.com>
2023-03-15 19:18:33 +0100

    homeScreen

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import Profile from './screens/Profile';
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          // options={{title: 'Welcome'}}
+          name="Profile"
+          component={Profile}
+          options={{
+            headerTransparent: true,
+            headerShown: false,
+            presentation: "modal"
+            
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  navigation.setOptions({
+    headerTitle: '',
+    headerStyle: { backgroundColor: '#0077b6' },
+    headerLeft: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+        <Ionicons name="person-circle-outline" size={28} color="white" style={{ marginLeft: 16 }} />
+      </TouchableOpacity>
+    ),
+  });
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+        <View style={styles.selector}>
+            <TouchableOpacity style={styles.selects}>
+                <Text style={{color:"#fff", fontSize: 20}}>Single player mode</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.selects}>
+                <Text style={{color:"#fff", fontSize: 20}}>Multiplayer mode</Text>
+            </TouchableOpacity>
+            
+        </View>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#0077b6",
+        width: "100%",
+        height: "100%",
+        display: 'flex',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    selector:{
+        gap: 10
+    },
+    selects:{
+        borderWidth: 5,
+        padding: 10,
+        borderColor: "#fff",
+        borderRadius: 5,
+        display: "flex",
+        alignItems: "center",
+
+    }
+})
+
+export default HomeScreen;
